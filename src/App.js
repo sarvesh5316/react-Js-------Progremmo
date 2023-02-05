6c5ef9719df4e12382a34f4780e602d35cc4529a
Sarvesh <sarveshgond0000@gmail.com>
2023-02-05 13:52:19 +0530

    abc

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React hello 
         </a>
       </header>
     </div>
